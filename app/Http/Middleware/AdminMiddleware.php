79cdce68651d3a31d038f01984a4183697792e9c
QuentinTestaferrata <133092199+QuentinTestaferrata@users.noreply.github.com>
2023-11-29 15:22:59 +0100

    Feature : Items in database
    Added items table to the database and seeded the DB with some items already

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -15,10 +15,9 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->check() && auth()->user()->role == 'admin') {
+        if (auth()->user()?->role == 'admin') {
             return $next($request);
         }
-    
         return redirect('/');
     }
 }
