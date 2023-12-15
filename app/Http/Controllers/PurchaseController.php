0fa4fe5c4e3de12a6b6d71184b7d571fb5979214
QuentinTestaferrata <133092199+QuentinTestaferrata@users.noreply.github.com>
2023-12-15 12:14:22 +0100

    Fix: Buy Items
    Fixed the buy items, since quantity wasn't calculated in the PurchasController, you always received only 1 item, even when selecting a higher quantity

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -10,33 +10,27 @@ use App\Models\Item;
 
 class PurchaseController extends Controller
 {
-    public function purchase($item_id)
-    {
+    public function purchase(Request $request, $item_id){
         $user = Auth::user();
         $item = Item::find($item_id);
-
-        // check als de loggedin user genoeg balance heeft
-        if ($user->balance >= $item->price) {
-            $user->balance -= $item->price;
+        $quantity = (int)$request->input('quantity');
+    //Balancecheck
+        if ($user->balance >= $item->price * $quantity && $quantity > 0) {
+            $user->balance -= $item->price * $quantity;
             $user->save();
-
-            // Check als de item al in inventory zit
+    //inventoryCheck
             $inventoryItem = $user->inventory()->where('item_id', $item->id)->first();
 
             if ($inventoryItem) {
-                $inventoryItem->quantity += 1;
+                $inventoryItem->quantity += $quantity;
                 $inventoryItem->save();
             } else {
                 $user->inventory()->create([
                     'item_id' => $item->id,
-                    'quantity' => 1,
+                    'quantity' => $quantity,
                 ]);
             }
-
-            return redirect()->route('home')->with('success', 'Item purchased successfully!');
-        } else {
-            return redirect()->route('home')->with('error', 'Insufficient balance to purchase the item.');
-        }
+            return redirect()->route('home')->with('success', 'Item(s) purchased successfully!');
+        } else {return redirect()->route('home')->with('error', 'Insufficient balance or invalid quantity to purchase the item.');}
     }
-
 }
